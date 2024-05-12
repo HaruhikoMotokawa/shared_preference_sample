1aa5ff8531878ced11d2b6c83b9c995cbad0266d
本川　晴彦 <h.motokawa@salop.co.jp>
2024-05-12 17:51:32 +0900

    feat: ファイルを移動

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preference_sample/custom_bottom_sheet.dart';
+import 'package:shared_preference_sample/my_home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: App()));
@@ -15,77 +16,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Flexible(
-                child: ListTile(
-                  title: Text('アイコンの表示・非表示'),
-                  trailing: Icon(Icons.light),
-                ),
-              ),
-              const Flexible(
-                child: ListTile(
-                  title: Text('背景色の番号'),
-                  trailing: Text('1'),
-                ),
-              ),
-              const Flexible(
-                child: ListTile(
-                  title: Text('タイトルの文字列'),
-                  trailing: Text('hoge'),
-                ),
-              ),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Row(
-                    children: [
-                      const Text('JSONで複数の設定'),
-                      const Spacer(),
-                      Expanded(
-                        child: Text(
-                          'text' * 20,
-                          softWrap: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const Divider(),
-              ElevatedButton(
-                child: const Text('アイコンの表示と非表示を切り替え'),
-                onPressed: () {},
-              ),
-              ElevatedButton(
-                child: const Text('背景色番号を設定'),
-                onPressed: () => showSelectColorBottomSheet(context),
-              ),
-              ElevatedButton(
-                child: const Text('タイトルを選択'),
-                onPressed: () => showSelectTitleBottomSheet(context),
-              ),
-              ElevatedButton(
-                child: const Text('複数の条件を設定'),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
