5294028e6a5fe60e77d4f8d093855a4cc851e582
本川　晴彦 <h.motokawa@salop.co.jp>
2024-05-26 17:17:18 +0900

    fix: コメントを追記

--- lib/domains/custom_setting.dart
+++ lib/domains/custom_setting.dart
@@ -1,12 +1,12 @@
-// ignore_for_file: public_member_api_docs
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'custom_setting.freezed.dart';
 part 'custom_setting.g.dart';
 
+/// カスタム設定
 @freezed
 class CustomSetting with _$CustomSetting {
+  /// カスタム設定
   // ignore: invalid_annotation_target
   @JsonSerializable(fieldRename: FieldRename.snake)
   const factory CustomSetting({
