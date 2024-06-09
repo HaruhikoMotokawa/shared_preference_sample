270796d5336160366852155a41105182427b6978
本川　晴彦 <h.motokawa@salop.co.jp>
2024-06-09 15:48:36 +0900

    chore: 一旦スキップするように追記

---
+++ test/presentations/my_home_page/my_home_page_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // late ProviderContainer container;
+  // late UncontrolledProviderScope scope;
+  test('', skip: true, () {});
+}
