7a12cf643c0abb203a5ecc061a55e095e35474be
本川　晴彦 <h.motokawa@salop.co.jp>
2024-06-09 16:15:55 +0900

    fix: 未使用のメソッドを削除

--- lib/presentations/my_home_page/my_home_page_view_model.dart
+++ lib/presentations/my_home_page/my_home_page_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preference_sample/data/repositories/key_value_repository/provider.dart';
-import 'package:shared_preference_sample/domains/custom_setting.dart';
 
 part 'my_home_page_view_model.g.dart';
 
@@ -21,10 +20,6 @@ class MyHomePageViewModel extends _$MyHomePageViewModel {
   /// タイトルを保存する
   Future<void> saveTitleText(String value) =>
       ref.read(keyValueRepositoryProvider).setTitleText(value);
-
-  /// CustomSettingを保存する
-  Future<void> saveCustomSetting(CustomSetting value) =>
-      ref.read(keyValueRepositoryProvider).setCustomSetting(value);
 
   /// shared_preferenceを初期化する
   Future<void> initAllData() => ref.read(keyValueRepositoryProvider).initData();
